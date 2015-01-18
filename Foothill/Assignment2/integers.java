1f37b60a5da5a1265260f8e871099d62a86a84db
Erich Sickert <esickert@gmail.com>
2015-01-17 19:42:12 -0800

    Java class assignment and stuff

--- Foothill/Assignment2/integers.java
+++ Foothill/Assignment2/integers.java
@@ -1,45 +1,43 @@
 class integers	{
 
     public static void main(String[] args)	{
-	int x = 2, y = 3, z = 2;
-	numbers(x,y,z);
-	smallest(x,y,z);
-	largest(x,y,z);
-    }
-
-    public static void numbers(int x, int y, int z)	{
-	int sum = (x + y + z);
-	int product = (x * y * z);
-	double average = ((double)sum / 3);
-	System.out.println("The sum is " + sum);
-	System.out.println("The product is " + product);
-	System.out.println("The average is " + average);
+	int x = 3, y = 4, z = 3;
+	
+	System.out.println("*******************************************");
+	calculate(x,y,z);
 
     }
-    public static void smallest(int x, int y, int z)	{
+
+    public static void calculate(int num1, int num2, int num3)	{
 	int temp = 0;
-	//WHAT IF THEY ARE EQUAL SHIT!!!!  OR IS THAT IMPORTANT
-	if (x < y)	{
-	    temp = x;
-	   // System.out.println(temp);
-	}
-	else temp = y;
-	if (temp < z)
-	    System.out.println(temp + " is the smallest");
-	else System.out.println(z + " is the smallest");
+
+	System.out.println("The sum is " + (num1 + num2 + num3));
+	System.out.println("The product is " + (num1 * num2 * num3));
+	System.out.println("The average is " + ((float)(num1 + num2 + num3) / 3));
+
+	if ((num1 == num2) && (num2 == num3))    // cheack if all inputs are equal!
+	    System.out.println("They are all " + num1 + "'s and equal!!!");
+	else    { 
+	    if (num1 > num2)	//checking if x greater than y
+	        temp = num1;
+	    else     
+	        temp = num2;
+	    if (temp > num3)
+		System.out.println(temp + " is the largest");
+	    else 
+		System.out.println(num3 + " is the largest");
+            if (num1 < num2)  //checking if x is less than y
+		temp = num1;
+	    else    
+		temp = num2;
+	    if (temp < num3)
+		System.out.println(temp + " is the smallest");
+		else    { 
+		System.out.println(num3 + " is the smallest");
+		System.out.println("******************************************");
+		}
+	}    
+	
     }
-    public static void largest(int x, int y, int z)	{
-	int temp = 0;
-	//WHAT IF THEY ARE EQUAL SHIT!!!! OR IS THAT IMPORTANT
-	if (x > y)	{
-	    temp = x;
-	   // System.out.println(temp);
-	}
-	else temp = y;
-	if (temp > z)
-	    System.out.println(temp + " is the largest");
-	else System.out.println(z + " is the largest");
-    }
-    
 }
 	
---
+++ Foothill/javaPlayground/test.java
@@ -0,0 +1,16 @@
+class test	{
+
+	public static void main(String[] args)	{
+
+	double x = -32.2;
+	boolean isPositive = (x > 0);
+	if (isPositive = true) 
+	    System.out.println(x + " is positive");
+	else                   
+	    System.out.println(x + " is not positive");
+
+	if (x > 0);
+	    System.out.println("positive");
+
+	}
+}

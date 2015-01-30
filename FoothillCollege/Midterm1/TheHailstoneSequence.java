0da709e9a24445e33dc6c51d6006e8ca926ce54b
Erich Sickert <esickert@gmail.com>
2015-01-29 23:32:25 -0800

    Added more Foothill java class projects

---
+++ FoothillCollege/Midterm1/TheHailstoneSequence.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TheHailstoneSequence	{
+    public static void main(String[] args)	{
+
+    Scanner userInput = new Scanner(System.in);
+    System.out.print("Input a number: ");
+    int inputNumber = userInput.nextInt();
+
+    System.out.println(inputNumber);
+
+    } // end of main
+} // end of TheHailstoneSequence
+    

73b270b04c14e81482b8d79160c73e1e1b7ed979
Erich Sickert <esickert@gmail.com>
2015-03-17 10:23:31 -0700

    fun with classes

--- FoothillCollege/ProblemSet7/Library.java
+++ FoothillCollege/ProblemSet7/Library.java
@@ -21,13 +21,36 @@ class Library    {
         return bookList;
     } //end of addBook()
   
+    public void borrowAvaliableBook(String bookTitle)  {
+         for (int i = 0; i < bookList.size(); i++)   {
+            if (bookList.get(i).title.equals(bookTitle))    {  
+                System.out.println(bookTitle + " is avaliable");
+                System.out.println("Borrowing " + bookTitle);
+                System.out.println("You've succesfully borrowed " + bookTitle); 
+                bookList.get(i).borrowed = true;
+            }
+//            else
+//                System.out.println(bookTitle + " is not in our catalog");
+        } // end of for loop
+    } // end of method
+
+    public void returnBook(String bookTitle)    {
+        for (int i = 0; i < bookList.size(); i++)   {
+            if (bookList.get(i).title.equals(bookTitle))    
+                bookList.get(i).borrowed = false;
+        }
+    } //end of method returnBook
+            
+
     public void printAvaliableBooks()    {
+        if (bookList.size() == 0)
+            System.out.println("No books in catalog"); 
         for (int i = 0; i < bookList.size(); i++)   {
             if (bookList.get(i).isBorrowed() == false)
                 System.out.println('"' + bookList.get(i).title + '"');
-            else    {
-                System.out.println("\"" + bookList.get(i).title + "\" - checked out.");
-            } //end of ifelse
+//            else    {
+//                System.out.println("\"" + bookList.get(i).title + "\" - checked out.");
+//            } //end of ifelse
         } //end of for loop
     } //end of method printAvaliableBooks
    
@@ -43,13 +66,7 @@ class Library    {
         firstLibrary.addBook(new Book("Le Petit Prince"));
         firstLibrary.addBook(new Book("The Lord of the Rings"));
         firstLibrary.addBook(new Book("A Tale of Two Cities"));
-        
- //       Book GWTW = new Book("Gone with the Wind");
         firstLibrary.addBook(new Book("Gone with the Wind"));
-//        firstLibrary.GWTW.borrowed = true;
-        firstLibrary.bookList.get(2).borrowed = true;
-        secondLibrary.addBook(new Book("Dune"));
-        secondLibrary.bookList.get(0).borrowed = true;
 
         System.out.println("Library addresses:");
         System.out.println(firstLibrary.address);
@@ -59,6 +76,26 @@ class Library    {
         System.out.println("Library hours:");        
         firstLibrary.printOpenHours();
         System.out.println(); 
+
+        System.out.println("Books available in the first library:");
+        firstLibrary.printAvaliableBooks();         //need to call method via firstLibrary
+
+        //firstLibrary.addBook(new Book("Gone with the Wind"));
+//        firstLibrary.bookList.get(2).borrowed = false;
+//        secondLibrary.addBook(new Book("Dune"));
+//        secondLibrary.bookList.get(0).borrowed = true;
+
+        firstLibrary.borrowAvaliableBook("Gone with the Wind");
+        System.out.println("********");
+        System.out.println("Books available in the first library:");
+        firstLibrary.printAvaliableBooks();         //need to call method via firstLibrary
+        System.out.println();
+        System.out.println("Returning  book");
+        System.out.println("********");
+        firstLibrary.returnBook("Gone with the Wind");
+
+
+
                 
         System.out.println("Books available in the first library:");
         firstLibrary.printAvaliableBooks();         //need to call method via firstLibrary

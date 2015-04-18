689a0aa70dea633393da85877a666f9375c12c7c
Erich Sickert <esickert@gmail.com>
2015-04-17 23:21:19 -0700

    Updated

---
+++ Temp/Test.java
@@ -0,0 +1,7 @@
+public class Test   {
+    public static void main(String[] args)  {
+
+        System.out.println("Hello world");
+        System.out.println("That's all folks!!");
+    } //end of main
+} //end of main

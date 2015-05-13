7695ad96fd89acbfe7b7ef7555d3c3efb95f5894
Erich Sickert <esickert@gmail.com>
2015-05-13 10:00:32 -0700

    More stuff

--- Temp/Circle.java
+++ Temp/Circle.java
@@ -1,15 +1,18 @@
 public class Circle {
     /** The radius of a circle */
     private double radius;
+    /** the number of objects created.  */
+    private static int numberOfObjects = 0;
 
     /** Construct a circle with radius 1 */
     Circle()    {
-//        radius = 1.0;
+        numberOfObjects++;
     }
 
     /** Construct a circle with assigned radius */
     Circle(double newRadius)    {
         radius = newRadius;
+        numberOfObjects++;
     }
 
     /** get radius  */
@@ -20,15 +23,19 @@ public class Circle {
     /** set a new radiuis  */
     public void setRadius(double newRadius) {       // conditional statement
         radius = (newRadius >= 0) ? newRadius : 0;  //this is the same as
-                                                    // if (newRadius >= 0)
+    }                                               // if (newRadius >= 0)
                                                     //     radius = newRadius;
                                                     // else
                                                     //     radius = 0;
-    }   
+       
 
     /** return the area of the ciircle  */
     public double findArea()    {
         return radius * radius * Math.PI;
     }
 
+    /** return the number of objects */
+    public static int getNumberOfObjects()  {
+        return numberOfObjects;
+    }
 } //end of class Circle

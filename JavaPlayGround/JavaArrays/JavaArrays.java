518243dbc0fdeb0aea2b9863a769e250debfdf83
esickert <esickert@gmail.com>
2015-06-30 17:39:26 -0700

    more

--- JavaPlayGround/JavaArrays/JavaArrays.java
+++ JavaPlayGround/JavaArrays/JavaArrays.java
@@ -1,16 +1,32 @@
 public class JavaArrays   {
 
-    public int[] myArray = new int[10];
+    private String[] myArray;
 
-    public int[][] myTwoDArray;
+    private String[][] my2DArray;
 
     JavaArrays()    {
     }
 
-    JavaArrays(int[] temp)   {
-        myArray = temp;
+    JavaArrays(String[][] temp)   {
+        my2DArray = temp;
     }
 
-
-
+    public void loadArray(String x)  {
+        for(int i = 0; i <= my2DArray.length -1; i++) {
+            for(int j = 0; j <= my2DArray[i].length - 1; j++)
+                my2DArray[i][j] = x;
+        }
+    }
+    
+    public void printArray()    {
+        for(int i = 0; i <= my2DArray.length - 1; i++)    {
+            System.out.println();
+            for (int j = 0; j <= my2DArray[i].length - 1; j++)
+                System.out.print(my2DArray[i][j]);
+        }
+        System.out.println();
+    }
+    
+//    public void 
+    
 }
--- JavaPlayGround/JavaArrays/TestJavaArrays.java
+++ JavaPlayGround/JavaArrays/TestJavaArrays.java
@@ -1,15 +1,11 @@
 public class TestJavaArrays {
     public static void main(String[] args)  {
 
-        JavaArrays myStuff = new JavaArrays();
+        String[][]  testArray2 = new String[2][3]; 
+        JavaArrays myStuff = new JavaArrays(testArray2);  //local array being passed to class JavaArray
 
-        for(int i = 0; i < 10; i++) {                             
-        myStuff.myArray[i] = i;
-        }
-
-        for(int i = 0; i < 10; i++) {
-            System.out.print(myStuff.myArray[i] + " ");
-        }
         System.out.println();
+        myStuff.loadArray("2D Array Dude");
+        myStuff.printArray();
     }
 }

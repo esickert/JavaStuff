99f1428a34b1d7ca4005aaeb1c60a935a0aad2ba
Erich Sickert <esickert@gmail.com>
2015-07-25 12:25:33 -0700

    more

--- JavaPlayGround/Array2String/TestArrayToString.java
+++ JavaPlayGround/Array2String/TestArrayToString.java
@@ -5,14 +5,12 @@ public class TestArrayToString    {
     public static void main(String[] args)  {
 
     double[] test1D = new double[5];
-<<<<<<< HEAD
+
     double[][] test2D = new double[3][3];
     ArrayToString test = new ArrayToString(test1D);
-=======
-    double[][] test2D = new double[3][3]; 
-    ArrayToString test = new ArrayToString(test2D);
-    
->>>>>>> 5d590c792e70d920bd3d44c250fa5e8895aacfdb
+
+
+
     
     System.out.println(test1D);
     test.print1DArray(test1D);

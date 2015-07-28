4322de3102dfcedf0525fcfed4b2456e0d49704c
Erich Sickert <esickert@gmail.com>
2015-07-28 16:43:02 -0700

    more

--- JavaPlayGround/Array2String/TestArrayToString.java
+++ JavaPlayGround/Array2String/TestArrayToString.java
@@ -5,21 +5,18 @@ public class TestArrayToString    {
     public static void main(String[] args)  {
 
     double[] test1D = new double[5];
-<<<<<<< HEAD
-    double[][] test2D = new double[3][3];
-    ArrayToString test = new ArrayToString(test1D);
-=======
-    double[][] test2D = new double[3][3]; 
-    ArrayToString test = new ArrayToString(test2D);
+
+    double[][][] test2D = new double[3][3][7];
+    ArrayToString test = new ArrayToString();
+
     
->>>>>>> 5d590c792e70d920bd3d44c250fa5e8895aacfdb
-    
-    System.out.println(test1D);
-    test.print1DArray(test1D);
-    test.print2DArray(test2D);
-    System.out.println(Arrays.deepToString(test2D) + "  ");  //Arrays is a class from util!!!!
+    System.out.println(test2D);
+//    test.print1DArray(test1D);
+//  test.print2DArray(test2D);
+    String stuff = Arrays.deepToString(test2D);
+    System.out.println(stuff);  //Arrays is a class from util!!!!
     System.out.println();
-    test.print2DArray(test2D);
+//    test.print2DArray(test2D);
     
     }
 }
